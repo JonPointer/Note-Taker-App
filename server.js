336d93578e6fba1b5cc7897967d1be92f6944bbc
Jon Pointer <jonpointer@msn.com>
2020-12-11 19:25:44 -0700

    Fixed error in index.js; set up HTML routes for / and /notes; set up API route for api/notes to retrieve notes from db

---
+++ server.js
@@ -0,0 +1,47 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set location of db file
+dbLocation = path.join(__dirname, 'db/db.json');
+
+// Initialize notesArray
+let notesArray = [];
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Set up the HTML routes
+// GET * should return the index.html file.
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+// GET /notes should return the notes.html file.
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+// Set up the API routes
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+    // Read the notes db into an array
+    // console.log(dbLocation);
+    fs.readFile(dbLocation, function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        // Set the notesArray equal to the returned data from the file
+        notesArray = JSON.parse(data);
+        console.log(notesArray);
+        // Respond with the notes array in JSON
+        res.json(notesArray);
+    });
+});
+
+
+
+// Bind and listen for connections on the specified host and port
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
