47b94c8abfa0886a5f72cc7a47aefe17737700e5
Jon Pointer <jonpointer@msn.com>
2020-12-13 22:26:29 -0700

    Finished POST api/notes route and fixed error in index.js in renderActiveNote.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cuid = require('cuid');
 
 // Sets up the Express App
 const app = express();
@@ -39,12 +40,36 @@ app.get('/api/notes', (req, res) => {
         // Respond with the notes array in JSON
         res.json(notesArray);
     });
-    // ?? Why can't res.json command be here?  If it's here, notesArray is empty, but it's a global variable
 });
 
-
-
-
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    const newNote = req.body;
+    // Set an ID parameter for the new note
+    let id = cuid();
+    newNote.id = id;
+    // Now, need to add this note to the db.json file
+    // First, read the array of note objects from the file
+    fs.readFile(dbLocation, function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        // Set the notesArray equal to the returned data from the file
+        notesArray = JSON.parse(data);
+        // Now add the new note to this array
+        notesArray = [...notesArray, newNote];
+        // Write this new array to the db file
+        fs.writeFile(dbLocation, JSON.stringify(notesArray), (err) => {
+            // throws an error, you could also catch it here
+            if (err) throw err;
+            // success case, the file was saved
+            console.log('New Note file Saved!');
+            // Now return the new array of notes
+            res.json(notesArray);
+        });
+    });
+});
 
 // Bind and listen for connections on the specified host and port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
